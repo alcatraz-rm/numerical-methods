7d7b0477b48d095770df5e3a7e1b4c07b8500b7c
German Yakimov <alcatraz@fctools.com>
2021-09-20 01:20:32 +0700

    Fix indicies problems.

--- pde/task-1/main.py
+++ pde/task-1/main.py
@@ -1,6 +1,7 @@
 import time
 
 import numpy as np
+from progress.bar import Bar
 from scipy import linalg
 
 
@@ -25,28 +26,21 @@ def u_exact(t, x):
 
 
 def u_table_exact(t_grid, x_grid):
-    result = np.zeros((len(t_grid), len(x_grid)))
-
-    for i, t in enumerate(t_grid):
-        for j, x in enumerate(x_grid):
-            result[i][j] = u_exact(t, x)
+    result = []
+    with Bar('Calculating exact function on grid:', max=(len(t_grid))) as bar:
+        for i in range(len(t_grid)):
+            result.append(np.array([u_exact(t_grid[i], x) for x in x_grid]))
+            bar.next()
 
     return result
 
 
 def find_max_dev(u, u_ex):
     arr = np.subtract(u, u_ex)
-    return max(abs(np.min(arr)), abs(np.max(arr))), np.average(arr)
+    return max(abs(np.min(arr)), abs(np.max(arr)))
 
 
 def solve(t_max, tau, x_max, h, a):
-    # t_max = 1.0
-    # tau = 10**(-4)
-    #
-    # x_max = 1.0
-    # h = 10**(-4)
-    # a = 0.027
-
     d = a / (h * h)
     p = 1 / tau
 
@@ -58,26 +52,30 @@ def solve(t_max, tau, x_max, h, a):
 
     u_ex = u_table_exact(t_grid, x_grid)
 
-    u = np.zeros((M, N))  # X ->;
+    u = np.zeros((M, N))
 
-    for i in range(N):
-        u[0][i] = mu(x_grid[i])
+    with Bar('Setting initial values:', max=N + 2 * M) as bar:
+        for i in range(N):
+            u[0][i] = mu(x_grid[i])
+            bar.next()
 
-    for i in range(M):
-        u[i][0] = mu_1(t_grid[i])
+        for i in range(M):
+            u[i][0] = mu_1(t_grid[i])
+            bar.next()
 
-    for i in range(M):
-        u[i][N - 1] = mu_2(t_grid[i])
+        for i in range(M):
+            u[i][N - 1] = mu_2(t_grid[i])
+            bar.next()
 
     matrix = np.zeros((M - 2, N - 2))
     b = np.zeros(M - 2)
 
-    matrix[0][0] = p + 2*d
+    matrix[0][0] = p + 2 * d
     matrix[0][1] = -d
 
     for i in range(1, N - 3):
         matrix[i][i - 1] = -d
-        matrix[i][i] = p + 2*d
+        matrix[i][i] = p + 2 * d
         matrix[i][i + 1] = -d
 
     matrix[N - 3][N - 4] = -d
@@ -91,28 +89,27 @@ def solve(t_max, tau, x_max, h, a):
             if 0 <= index < 3:
                 matrix_prep[index][j] = matrix[i][j]
 
-    for s in range(M - 1):
-        print(s)
+    with Bar('Solving (time layer):', max=M-1) as bar:
+        for s in range(M - 1):
+            b[0] = u[s][1] * p + d * u[s + 1][0] + f(x_grid[1], t_grid[s + 1])
 
-        b[0] = u[s][1] * p + d * u[s+1][0] + f(x_grid[1], t_grid[s+1])
+            for i in range(1, N - 3):
+                b[i] = u[s][i + 1] * p + f(x_grid[i + 1], t_grid[s + 1])
 
-        for i in range(2, N - 2):
-            b[i - 1] = u[s][i] * p + f(x_grid[i], t_grid[s + 1])
+            b[N - 3] = u[s][N - 2] * p + d * u[s + 1][N - 1] + f(x_grid[N - 2], t_grid[s + 1])
 
-        # FIXME: if N != M?
+            vec = linalg.solve_banded((1, 1), matrix_prep, b)
 
-        b[N - 3] = u[s][N-1] * p + d * u[s+1][N] + f(x_grid[N - 1], t_grid[s + 1])
+            for k in range(1, N - 1):
+                u[s + 1][k] = vec[k - 1]
 
-        vec = linalg.solve_banded((1, 1), matrix_prep, b)
+            bar.next()
 
-        for k in range(1, N-1):
-            u[s + 1][k] = vec[k - 1]
-
-    print(find_max_dev(u, u_ex))
+    print("\nMax deviation:", find_max_dev(u, u_ex))
 
 
 start_time = time.time()
-solve(1, 1 / 10000, 1, 1 / 10000, 0.027)
+solve(1, 1 / 50000, 1, 1 / 50000, 0.027)
 end_time = time.time()
 
-print((end_time - start_time))
+print("Elapsed seconds:", (end_time - start_time))
